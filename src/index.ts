216755356eebe65ea232721c3d1005e9d762dbf9
Simon Holloway <simon@syhol.io>
2021-05-30 18:16:32 +0100

    chore: lowercase decorators - part 1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './Config';
 export * from './ConfigDefinition';
 export * from './ConfigContext';
-export * from './Decorators';
+export * from './decorators1';
 export * from './facade';
 export * from './ConfigResolver';
